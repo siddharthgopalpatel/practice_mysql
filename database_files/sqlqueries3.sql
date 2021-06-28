17ecf18e24138bd936a6c29f785e3d5decdf2503
sidd <sidd@gmail.com>
2021-06-28 00:10:08 -0500

    version 8

---
+++ database_files/sqlqueries3.sql
@@ -0,0 +1,3 @@
+CREATE TABLE Players_list (Player VARCHAR(255), Span INT, Mat INT, Inning INT, Notout INT, Runs INT, HS INT, Avg INT, Centuary INT, Half-Centuary INT, Zero INT, PRIMARY KEY (RUNS));
+
+LOAD DATA INFILE '/var/lib/mysql-files/cricket_odi.csv' INTO TABLE Customer FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
